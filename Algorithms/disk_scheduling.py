6f4b65b930a2e2403d57823540a86d7e182f9388
Mulayam <mulyam13986@gmail.com>
2023-11-08 22:50:35 +0530

    fcfs,sstf,scan:disk scheduling

---
+++ Algorithms/disk_scheduling.py
@@ -0,0 +1,82 @@
+def first_come_first_serve(sequence , head_position):
+
+    seek_count =0
+    prev = head_position 
+    for i in sequence:
+        seek_count += abs(i-prev)
+        prev = i
+    sequence_track = [head_position] + sequence
+    return sequence_track ,seek_count 
+
+def sstf(sequence , head_position):
+
+    curr_head = head_position 
+    cnt = len(sequence)
+    visited = [0]*cnt
+    sequence_track = [head_position]
+    total_seek_time = 0 
+    for i in range(cnt):
+
+        min_seek = 1e9
+        idx = -1
+        for i in range(len(sequence)):
+
+            if visited[i] == 1 :
+                continue
+            else :
+                seek_time = abs(curr_head - sequence[i])
+                if seek_time < min_seek :
+                    min_seek = seek_time 
+                    idx = i 
+        sequence_track.append(sequence[idx]) 
+        total_seek_time += abs(sequence[idx] - curr_head)
+        curr_head = sequence[idx]
+        visited[idx] = 1
+    
+    return sequence_track , total_seek_time 
+
+def scan(sequence , head , direction , disk_size):
+    sequence_track = []
+    left_track  = []
+    right_track = []
+    sequence.sort() 
+    
+    for i in sequence:
+        if i < head :
+            left_track.append(i)
+        else :
+            right_track.append(i)
+    if (direction == "LEFT"): 
+        
+        left_track.reverse()
+        left_track.append(0)
+        sequence_track = left_track + right_track
+        total_seek_time = head + right_track[-1]
+    
+    else :
+        # direction = RIGHT 
+        right_track.append(disk_size)
+        left_track.reverse()
+        sequence_track = right_track + left_track 
+        total_seek_time = disk_size-head + disk_size-left_track[-1]
+    
+    return sequence_track , total_seek_time 
+
+def cscan(sequence , head , direction , disk_size):
+    pass 
+
+
+
+queue = [98,183,37,122,14,124,65,67] 
+head = 53
+disk_size = 199 
+
+print(first_come_first_serve(queue,head))
+print(sstf(queue,head))
+print(scan(queue,head,"RIGHT",disk_size))
+
+
+
+        
+
+
