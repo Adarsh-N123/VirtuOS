568c715756ae6d7aace91770bc84c7195c08c0d0
Mulayam <mulyam13986@gmail.com>
2023-11-09 00:45:26 +0530

    updated

--- Algorithms/disk_scheduling.py
+++ Algorithms/disk_scheduling.py
@@ -42,38 +42,59 @@ def scan(sequence , head , direction , disk_size):
     sequence.sort() 
     
     for i in sequence:
-        if i < head :
+        if i <= head :
             left_track.append(i)
         else :
             right_track.append(i)
     if (direction == "LEFT"): 
         
         left_track.reverse()
-        left_track.append(0)
+        if (left_track == [] or left_track[-1] != 0):
+            left_track.append(0)
         sequence_track = left_track + right_track
-        total_seek_time = head + right_track[-1]
+        total_seek_time = head 
+        if (right_track != []):
+            total_seek_time+= right_track[-1]
     
     else :
         # direction = RIGHT 
-        right_track.append(disk_size)
+        if (right_track == [] or right_track[-1] != disk_size-1) :
+            right_track.append(disk_size-1)
         left_track.reverse()
         sequence_track = right_track + left_track 
-        total_seek_time = disk_size-head + disk_size-left_track[-1]
+        total_seek_time = disk_size-1-head 
+        
+        if (left_track != []):
+            total_seek_time += disk_size-1-left_track[-1]
     
     return sequence_track , total_seek_time 
 
-def cscan(sequence , head , direction , disk_size):
-    pass 
+def cscan(sequence , head , disk_size):
+    sequence_track = []
+    left_track  = []
+    right_track = []
+    sequence.sort() 
+    
+    for i in sequence:
+        if i <= head :
+            left_track.append(i)
+        else :
+            right_track.append(i)
+    total_seek_distance = 0 
+    if (right_track == [] or right_track[-1] != disk_size):
+        right_track.append(disk_size)
+    
+    
 
 
 
-queue = [98,183,37,122,14,124,65,67] 
-head = 53
+queue = [176, 79, 34, 60,92, 11, 41, 114] 
+head = 50
 disk_size = 199 
 
-print(first_come_first_serve(queue,head))
-print(sstf(queue,head))
-print(scan(queue,head,"RIGHT",disk_size))
+# print(first_come_first_serve(queue,head))
+# print(sstf(queue,head))
+print(scan(queue,head,"LEFT",disk_size))
 
 
 
