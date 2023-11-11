613d48b4c2d00e9c555f619669b2c3db284207bb
NIDAMANURI SAI ADARSH <adarshkln5@gmail.com>
2023-11-12 03:03:12 +0530

    Date and Day

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./nav.css"
 import Help from './help'
 import Simulationwindow from './Simulationwindow'
@@ -7,6 +7,50 @@ function Nav(props) {
     const options = ["Window","Help","Simulations","Preferences"]
     const optionssim = ["FCFS","SJF","SRTF","Priority","Round Robin","Bankers Algorithm","Page Replacement Algorithms","Disk Scheduling Algorithms","Memory Allocation Algorithms","File Allocation Algorithms","Deadlock"]
     const [help,sethelp] = useState(false);
+    // 
+    // Create a new Date object
+    const [formattedDate, setFormattedDate] = useState('');
+
+  useEffect(() => {
+    console.log(formattedDate);
+  }, [formattedDate]);
+
+  useEffect(() => {
+    function updateDate() {
+      let currentDate = new Date();
+
+      let options = {
+        weekday: 'short',
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+      };
+
+      let newFormattedDate = new Intl.DateTimeFormat('en-US', options).format(currentDate);
+      setFormattedDate(newFormattedDate);
+    }
+
+    // Update the date initially
+    updateDate();
+
+    // Set up an interval to update the date every minute (60,000 milliseconds)
+    const intervalId = setInterval(updateDate, 60000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
+  
+    
+    // console.log(`Formatted Date: ${formattedDate}`);
+    
+
+
+
+
+
+// 
     const openwindow = (folder) => {
       const updatedWindows = [...props.windows]; // Create a copy of the windows array
       for (let i = 0; i < updatedWindows.length; i++) {
@@ -43,6 +87,7 @@ function Nav(props) {
   return (
     <div>
     <div className="nav">
+      <div className='nav1'>
       <img src='https://p3d.in/static/uploads/94995/image-db241279986.png' style={{width:'40px',marginLeft:'10px',borderRadius:'10%'}}></img>
       {options.map((option) => (
                 <div key={option.toString()} id={option}>
@@ -57,6 +102,14 @@ function Nav(props) {
     </div>
                 </div>
             ))}
+            </div>
+      <div style={{display:'flex',alignItems:'center',justifyContent:'end'}}>
+        <img id='controlcenter' src='https://upload.wikimedia.org/wikipedia/en/1/1e/Control_Center_%28Apple%29.png' style={{height:'23px',marginRight:'8px',marginTop:'2px',cursor:'pointer'}}></img>
+        
+      <div style={{width:'150px',fontSize:'11px',color:'black',fontWeight:'600',marginRight:'10px'}}>
+        {formattedDate}
+      </div>
+      </div>
         
 
     </div>
